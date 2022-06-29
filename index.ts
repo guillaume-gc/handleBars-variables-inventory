40a2bbaefc08e73a1385de8a504a7b1b5abafe6d
Guillaume Carmelli <guillaume.carmelli@partnre.com>
2022-06-29 09:36:42 +0200

    feat: add basic prototype

---
+++ index.ts
@@ -0,0 +1,25 @@
+import Handlebars from "handlebars"
+import {readFileSync} from "fs";
+import {join} from "path";
+
+const getHandlebarsVariables = (input: string): string[] => {
+    const ast: hbs.AST.Program = Handlebars.parseWithoutProcessing(input)
+
+    return ast.body.filter(({ type }: hbs.AST.Statement) => (
+        type === 'MustacheStatement'
+    ))
+        .map((statement: hbs.AST.Statement) => {
+            const moustacheStatement: hbs.AST.MustacheStatement = statement as hbs.AST.MustacheStatement
+            const paramsExpressionList = moustacheStatement.params as hbs.AST.PathExpression[]
+            const pathExpression = moustacheStatement.path as hbs.AST.PathExpression
+
+            return paramsExpressionList[0]?.original || pathExpression.original
+        })
+}
+
+const path = join(__dirname, '/../sample/sample_all.html')
+const sampleData = readFileSync(path, 'utf8')
+
+const result = getHandlebarsVariables(sampleData)
+
+console.log(result)
